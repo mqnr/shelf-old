211eeccf0c7b88df56f615b63a7de803d997f781
mzacuna <80491699+mzacuna@users.noreply.github.com>
2024-04-30 20:19:34 -0700

    add basic implementation for IPersistenciaRevistas

---
+++ bibliotecaPersistencia/src/main/java/com/mzamorano/school/bibliotecapersistencia/PersistenciaRevistas.java
@@ -0,0 +1,40 @@
+package com.mzamorano.school.bibliotecapersistencia;
+
+import com.mzamorano.school.objetosnegocio.Revista;
+
+import java.util.HashMap;
+
+public class PersistenciaRevistas implements IPersistenciaRevistas {
+    private final HashMap<String, Revista> revistas = new HashMap<>();
+
+    @Override
+    public boolean agregar(Revista revista) {
+        if (revista == null) {
+            throw new IllegalArgumentException("Valores null no permitidos");
+        }
+        return revistas.putIfAbsent(revista.getIsbn(), revista) == null;
+    }
+
+    @Override
+    public Revista obtener(String isbn) {
+        return revistas.get(isbn);
+    }
+
+    @Override
+    public boolean actualizar(Revista revista) {
+        if (revista == null) {
+            throw new IllegalArgumentException("Valores null no permitidos");
+        }
+        return revistas.replace(revista.getIsbn(), revista) != null;
+    }
+
+    @Override
+    public boolean eliminar(String isbn) {
+        return revistas.remove(isbn) != null;
+    }
+
+    @Override
+    public boolean eliminar(Revista revista) {
+        return revistas.remove(revista.getIsbn()) != null;
+    }
+}
