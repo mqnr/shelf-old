6298ac1e673f0a2040467de049ac36d8ddd6e98b
mzacuna <80491699+mzacuna@users.noreply.github.com>
2024-04-30 20:18:30 -0700

    tweak IPersistenciaRevistas

--- bibliotecaPersistencia/src/main/java/com/mzamorano/school/bibliotecapersistencia/IPersistenciaRevistas.java
+++ bibliotecaPersistencia/src/main/java/com/mzamorano/school/bibliotecapersistencia/IPersistenciaRevistas.java
@@ -3,9 +3,9 @@ package com.mzamorano.school.bibliotecapersistencia;
 import com.mzamorano.school.objetosnegocio.Revista;
 
 public interface IPersistenciaRevistas {
-    void agregar(Revista revista);
-    Revista obtenerPorIsbn(String isbn);
-    void actualizar(Revista revista);
-    void eliminar(Revista revista);
-    void eliminarPorIsbn(String isbn);
+    boolean agregar(Revista revista);
+    Revista obtener(String isbn);
+    boolean actualizar(Revista revista);
+    boolean eliminar(String isbn);
+    boolean eliminar(Revista revista);
 }
